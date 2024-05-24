32b6c0a4465755125f5120017a6be8d2ec402157
Denes Milan Kende <9d3b3z@gmail.com>
2024-05-24 12:53:34 +0200

    maze almost done
    only problem is that it goes back : (

--- maze_solver/main.py
+++ maze_solver/main.py
@@ -18,34 +18,77 @@ labs3 = input.split("C")[1].replace(" ","").split("\n")
 labs3.pop(0)
 labs3.pop(len(labs3)-1)
 
-print(labs1)
-print(labs2)
-print(labs3)
-
 
 def maze_solver(map:list):
-    pos = []
-    answ = [""]
+    positions = []
+    new_positions = []
+    answ = ["S"]
     check_around_nums = [-1,1]
+    check_around_pos_num = [[0,-1],[-1,0],[0,1],[1,0]]
     check_around_directions = ["U","L","D","R"]
     check_around_elements = []  # fent, bal, lent, jobb
+    done = False
 
     for i,e in enumerate(map):
         if e.count("S") > 0:
-            pos.append([e.index("S"), i])
+            positions.append([e.index("S"), i])
             break
 
-    for i in pos:
-        for k in check_around_nums:
-            check_around_elements.append(map[i[1]+k][i[0]])
-            check_around_elements.append(map[i[1]][i[0]+k])
-        if check_around_elements.count(".") == 1:
-            answ[0] += check_around_directions[check_around_elements.index(".")]
-            pos[0]
+    while True:
+        for i,e in enumerate(positions):
+            for k in check_around_nums:
+                check_around_elements.append(map[e[1]+k][e[0]])
+                check_around_elements.append(map[e[1]][e[0]+k])
 
+            if check_around_elements.count("G") == 1:
+                answ[i] += check_around_directions[check_around_elements.index(".")] + "G"
+                print(answ)
+                done = True
+
+            if check_around_elements.count(".") == 1:
+                if not answ[i][-1] == "S":
+                    previous_move = answ[i][-1]
+                    index_of_direction_of_previous_move = check_around_directions.index(previous_move)
+                    if index_of_direction_of_previous_move > 1:
+                        opposite_of_previous_move = check_around_directions[index_of_direction_of_previous_move-2]
+                    else:
+                        opposite_of_previous_move = check_around_directions[index_of_direction_of_previous_move+2]
+                    if opposite_of_previous_move == check_around_directions[check_around_elements.index(".")]:  # zsákutca?
+                        answ.pop(i)
+                        positions.pop(i)
+                        break
+
+                answ[i] += check_around_directions[check_around_elements.index(".")]
+                l = check_around_pos_num[check_around_directions.index(answ[i][-1])]
+                new_positions.append([e[0] + l[0], e[1] + l[1]])
+
+            elif check_around_elements.count(".") > 1:
+                new_pos_not_needed = True
+                for i_,e_ in enumerate(check_around_elements):
+                    if e_ == ".":
+                        if new_pos_not_needed:
+                            answ[i] += check_around_directions[i_]
+                            l = check_around_pos_num[check_around_directions.index(answ[i][-1])]
+                            new_positions.append([e[0] + l[0], e[1] + l[1]])
+                            new_pos_not_needed = False
+                        else:
+                            new_positions.append(e)
+                            new_pos = new_positions[-1]
+                            index_new_pos = new_positions.index(new_pos)
+                            answ.append(answ[i][:-1] + check_around_directions[i_])
+                            l = check_around_pos_num[check_around_directions.index(answ[index_new_pos][-1])]
+                            new_positions[index_new_pos] = [e[0] + l[0], e[1] + l[1]]
+
+            check_around_elements.clear()
+
+        if done:
+            break
+        positions = new_positions.copy()
+        new_positions.clear()
 
 
 
 maze_solver(labs1)
 
 end = time.time()
+print(end - start)
