ad6566bfab2a477fecd2a7d003a68b5965fdfea6
D3B3ZGYP <9d3b3z@gmail.com>
2024-05-27 19:13:07 +0200

    kvíz

--- math_quiz/main.py
+++ math_quiz/main.py
@@ -1,41 +1,9 @@
-# 1. feladat
-lampa_ido = 5
-alagut_hossz = 300
-ido = 20
-
-sebesseg = alagut_hossz / (ido - lampa_ido)
-vonat_hossz = sebesseg * lampa_ido
-print(f'1.:{vonat_hossz}')
-
-# 2. feladat
+print(f"1.:100")
 print(f"2.:")
-
-# 3. feladat
 print(f"3.:")
-
-# 4. feladat
-print(f"4.:")
-
-# 5. feladat
+print(f"4.:12")
 print(f"5.:")
-
-# 6. feladat
 print(f"6.:")
-
-# 7. feladat
 print(f"7.:")
-
-# 8.feladat
-atlokat_erintett_negyzetek = 85
-
-if atlokat_erintett_negyzetek % 2 == 0:
-    atlot_erinto_negyzetek = atlokat_erintett_negyzetek/2
-else:
-    atlot_erinto_negyzetek = (atlokat_erintett_negyzetek+1)/2
-print(f"8.:{atlot_erinto_negyzetek}")
-
-# 9. feladat
-goodbye = 198
-viszlat = 308
-
-angolok =
+print(f"8.:1849")
+print(f"9.:9")
